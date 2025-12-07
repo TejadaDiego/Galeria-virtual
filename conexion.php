339d9a78acbd1c5898f933ca7c13479bc4530e68
Diego Tejada Espinal <Diego.tejada2006@gmail.com>
2025-12-06 23:41:25 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 
 $servername = "localhost";   
 $username = "root";
-$password = "dieguito.2006";
+$password = "";
 $dbname   = "galeria_virtual";
 $port     = 3307; // usa 3306 si tu MySQL está ahí
 
