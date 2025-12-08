e3dd3480aec0004e2b2d324d17a108a85bea684d
Diego Tejada Espinal <Diego.tejada2006@gmail.com>
2025-12-08 14:36:48 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,22 +1,16 @@
 <?php
-// Php/conexion.php
 
 $servername = "127.0.0.1";
 $username   = "root";
 $password   = "dieguito.2006"; 
 $dbname     = "galeria_virtual";
-$port       = 3307;
+$port       = 9516; // EL PUERTO REAL DE TU MYSQL AHORA
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Verificar
 if ($conn->connect_error) {
-    http_response_code(500);
-    die(json_encode([
-        "ok"  => false,
-        "msg" => "Error al conectar con la base de datos: " . $conn->connect_error
-    ]));
+    die("Error de conexión: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
+?>
